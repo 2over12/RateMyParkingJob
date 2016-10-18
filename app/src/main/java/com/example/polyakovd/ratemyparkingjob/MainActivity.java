96e1d434d16ac0f97ac39d68c61daf317e5dc269
Denpoly <polyakovd@MA-14-POLYA18.local>
2016-10-18 18:09:28 -0400

    First version of camera feature
    Still need to fully implement picture taking feature…
    Things to do with the camera feature:
    -display as full sized picture, not icon
    -save it to local storage
    -bring user to different activity to confirm post

--- app/src/main/java/com/example/polyakovd/ratemyparkingjob/MainActivity.java
+++ app/src/main/java/com/example/polyakovd/ratemyparkingjob/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.polyakovd.ratemyparkingjob;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FloatingActionButton newPostBtn = (FloatingActionButton) findViewById(R.id.newPost); //Variable for the New Post Fab and setting its onClick listener.
+
+        assert newPostBtn != null;
+        newPostBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
     }
+
+    //Method for passing intent to capturing a picture using camera app.
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    //This method is used for getting photo back from the above intent.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView mImageView = (ImageView) findViewById(R.id.imageView);
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+
+
 }
